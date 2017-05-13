81b107d8aea2fd882fc6f1999d3780d8248a3662
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-05-13 16:51:49 -0500

    Added trap, more info on ecga

--- eda/ecga.go
+++ eda/ecga.go
@@ -22,3 +22,17 @@ func ECGAModel(opts ...Option) (Model, error) {
 	// Build MPM Model using MDL???
 	return ecga, err
 }
+
+// ECGA isn't terribly hard to do-- we already have crossover and selection
+// covered, and elitism is fine but we should probably give the other models
+// who use populations elitism if we give ECGA elitism. The one hard thing
+// is the MPM model which in itself isn't hard but requires that we split
+// the problem space up into building blocks, and requires that we learn these
+// building blocks, I guess, although I'm not certain on that
+//
+// Yes, according to paper 1 we begin the MDM model with the assumption that
+// every index is its own building block, and sees if merging all pairs is 
+// helpful. 
+//
+// Papers : https://pdfs.semanticscholar.org/eeee/a9fdade929cb3fc9a99631d3541ef7005079.pdf
+// http://www.kumarasastry.com/wp-content/files/2000026.pdf
---
+++ eda/fitness/trap.go
@@ -0,0 +1,44 @@
+package fitness
+
+import (
+	"math/rand"
+
+	"bitbucket.org/StephenPatrick/goevo/env"
+)
+
+// TrapABS takes in the bitlength t of the trap
+// trap fitnesses follow reward worse fitness for
+// increasing numbers of ones in subgroups of the environment
+// until all elements in the subgroup are one, at which
+// point the reward the best fitness.
+// A graph:
+//
+// Good fitness  |                             -
+//               |--------
+//               |        -----------
+//               |                   ----------
+//               |------------------------------
+// Poor Fitness   No ones              More ones
+//
+func TrapABS(t int) func(e *env.F) int {
+	return func(e *env.F) int {
+		diff := 0
+		for i := 0; i < len(*e); i += t {
+			ones := 0
+			for j := i; j < i+t; j++ {
+				if rand.Float64() < e.Get(j) {
+					ones++
+				}
+			}
+			diff += trap(t, ones)
+		}
+		return diff
+	}
+}
+
+func trap(t, ones int) int {
+	if t == ones {
+		return 0
+	}
+	return ones + 1
+}
