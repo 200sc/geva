94e68872e74a889b82a3e66fb79e305f77e8d6fc
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-05-12 19:48:44 -0500

    Added sixpeaks

---
+++ eda/fitness_peaks.go
@@ -0,0 +1,101 @@
+package eda
+
+import (
+	"math"
+	"math/rand"
+
+	"bitbucket.org/StephenPatrick/goevo/env"
+)
+
+// FourPeaks represents a problem where there are four explicit
+// maxima in the search space and two of the maxima can hide the
+// other two.
+func FourPeaks(t int) func(m Model) int {
+	return func(m Model) int {
+		e := m.ToEnv()
+		leadingOnes := 0
+		for _, f := range *e {
+			if rand.Float64() < *f {
+				leadingOnes++
+			} else {
+				break
+			}
+		}
+		trailingZeroes := 0
+		for i := len(*e) - 1; i >= 0; i-- {
+			f := (*e)[i]
+			if rand.Float64() > *f {
+				trailingZeroes++
+			} else {
+				break
+			}
+		}
+		base := int(math.Max(float64(leadingOnes), float64(trailingZeroes)))
+		if trailingZeroes > t && leadingOnes > t {
+			base += len(*e)
+		}
+		return ((2 * len(*e)) - t) - base
+	}
+}
+
+func SixPeaks(t int) func(m Model) int {
+	return func(m Model) int {
+		e := m.ToEnv()
+		leadingOnes, leadingZeroes, trailingOnes, trailingZeroes := bsEndlengths(e)
+		base := int(math.Max(float64(leadingOnes), float64(trailingZeroes)))
+		if (trailingZeroes > t && leadingOnes > t) ||
+			(trailingOnes > t && leadingZeroes > t) {
+			base += len(*e)
+		}
+		return ((2 * len(*e)) - t) - base
+	}
+}
+
+func bsEndlengths(e *env.F) (int, int, int, int) {
+	leadingOnes := 0
+	leadingZeroes := 0
+	if rand.Float64() < e.Get(0) {
+		leadingOnes++
+		for i := 1; i < len(*e); i++ {
+			if rand.Float64() < e.Get(i) {
+				leadingOnes++
+			} else {
+				break
+			}
+		}
+	} else {
+		leadingZeroes++
+		for i := 1; i < len(*e); i++ {
+			if rand.Float64() > e.Get(i) {
+				leadingZeroes++
+			} else {
+				break
+			}
+		}
+	}
+	trailingOnes := 0
+	trailingZeroes := 0
+	if rand.Float64() < e.Get(len(*e)-1) {
+		trailingOnes++
+		for i := len(*e) - 2; i > -1; i-- {
+			if rand.Float64() < e.Get(i) {
+				trailingOnes++
+			} else {
+				break
+			}
+		}
+	} else {
+		trailingZeroes++
+		for i := len(*e) - 2; i > -1; i-- {
+			if rand.Float64() > e.Get(i) {
+				trailingZeroes++
+			} else {
+				break
+			}
+		}
+	}
+	return leadingOnes, leadingZeroes, trailingOnes, trailingZeroes
+}
+
+// Related problems:
+// K-Coloring
--- eda/mimic_test.go
+++ eda/mimic_test.go
@@ -30,3 +30,25 @@ func TestFourPeaksMIMIC(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotNil(t, model)
 }
+
+func TestSixPeaksMIMIC(t *testing.T) {
+	fmt.Println("SixPeakMIMIC")
+	Seed()
+	length := 100.0
+	model, err := Loop(MIMICModel,
+		BenchTest,
+		FitnessFunc(SixPeaks(int(length/10))),
+		Length(int(length)),
+		LearningRate(0.07),
+		MutationRate(.15),
+		FMutator(
+			mut.And(
+				mut.Or(
+					mut.Or(mut.Add(.1), mut.Add(-.1), .5),
+					mut.DropOut(0.5), .99),
+				EnforceRange(floatrange.NewLinear(0.0, 1.0))),
+		),
+	)
+	assert.Nil(t, err)
+	assert.NotNil(t, model)
+}
